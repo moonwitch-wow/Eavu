de8d0de4780a03564f77bb608673fe92764c670b
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-05 00:55:56 +0100

    Changed Broker Addons

--- modules/broker.lua
+++ modules/broker.lua
@@ -1,20 +1,20 @@
 -- left side
-local guild = LibStub('LibCargoShip-2.1'):CreateBlock('picoGuild', {
+local guild = LibStub('LibCargoShip-2.1'):CreateBlock('SocialState', {
 	})
 guild:SetPoint("LEFT", BottomPanel, "LEFT", 10, 0)
 
-local friends = LibStub('LibCargoShip-2.1'):CreateBlock('picoFriends', {
-	})
-friends:SetPoint("LEFT", guild, "RIGHT", 40, 0)
+-- local friends = LibStub('LibCargoShip-2.1'):CreateBlock('Folks_Friends', {
+-- 	})
+-- friends:SetPoint("LEFT", guild, "RIGHT", 40, 0)
 
-local dura= LibStub("LibCargoShip-2.1"):CreateBlock("tekability", {
+local dura= LibStub("LibCargoShip-2.1"):CreateBlock("Durability", {
 	})
-dura:SetPoint("LEFT", friends, "RIGHT", 40, 0)
+dura:SetPoint("LEFT", guild, "RIGHT", 40, 0)
 
-local tg = LibStub("LibCargoShip-2.1"):CreateBlock("TourGuide", {
-	width = 100,
-	})
-tg:SetPoint("LEFT", dura, "RIGHT", 40, 0)
+-- local tg = LibStub("LibCargoShip-2.1"):CreateBlock("TourGuide", {
+-- 	width = 100,
+-- 	})
+-- tg:SetPoint("LEFT", dura, "RIGHT", 40, 0)
 
 -- right side
 local money = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Money", {
@@ -30,14 +30,14 @@ local bags = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Bags", {
 	})
 bags:SetPoint("RIGHT", fps, "LEFT", -40, 0)
 
-if select(2, UnitClass("player")) == "SHAMAN" then 
+if select(2, UnitClass("player")) == "SHAMAN" then
 	local ankh = LibStub("LibCargoShip-2.1"):CreateBlock("AnkhUp", {
 		})
 	ankh:SetPoint("RIGHT", bags, "LEFT", -40, 0)
 end
 
 -- centered
-local dps = LibStub('LibCargoShip-2.1'):CreateBlock('picoDPS', {fontSize=10})
+local dps = LibStub('LibCargoShip-2.1'):CreateBlock('DPS', {fontSize=10})
 dps:SetPoint("RIGHT", BottomPanel, "CENTER", -250, 0)
 
 local group = LibStub("LibCargoShip-2.1"):CreateBlock("picoGroup", {fontSize = 10})
