5fa3d67563be141a080c82c805d31f5020f2bab6
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-06 20:35:03 +0100

    indentation my dear watson

--- modules/combatlog.lua
+++ modules/combatlog.lua
@@ -1,18 +1,18 @@
 LibStub("LibDataBroker-1.1"):NewDataObject("CombatLogToggle", {
-	type = "launcher",
-	icon = [[Interface\Icons\INV_Weapon_ShortBlade_01]],
-	OnClick = function()
-		if not COMBATLOG then
-			DEFAULT_CHAT_FRAME:AddMessage("ERROR - COMBATLOG frame does not exist.")
-			return
-		end
-		local tab = _G[COMBATLOG:GetName().."Tab"]
-		if COMBATLOG:IsVisible() then
-			COMBATLOG:Hide()
-			tab:Hide()
-		else
-			COMBATLOG:Show()
-			tab:Show()
-		end
-	end
+  type = "launcher",
+  icon = [[Interface\Icons\INV_Weapon_ShortBlade_01]],
+  OnClick = function()
+    if not COMBATLOG then
+      DEFAULT_CHAT_FRAME:AddMessage("ERROR - COMBATLOG frame does not exist.")
+      return
+    end
+    local tab = _G[COMBATLOG:GetName().."Tab"]
+    if COMBATLOG:IsVisible() then
+      COMBATLOG:Hide()
+      tab:Hide()
+    else
+      COMBATLOG:Show()
+      tab:Show()
+    end
+  end
 })
