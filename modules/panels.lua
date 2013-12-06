70eba4c338f321ba7acbdd916148e18604544561
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-06 21:59:07 +0100

    changed the function to not populate the table Eavu with funds that are not events

--- modules/panels.lua
+++ modules/panels.lua
@@ -4,14 +4,14 @@ local __, Eavu = ...
 -- Panel Creation - Bottom
 -- Syntax Eavu.CreatePanel(frameName, width, height, anchor1, parent, anchor2, x, y)
 --------------------------------------------
-Eavu.CreatePanel("BottomPanel" , UIParent:GetWidth()-10, EavuDB["panels"].height, "BOTTOM", UIParent, "BOTTOM", 0, 5)
+CreateEavuPanel("BottomPanel" , UIParent:GetWidth()-10, EavuDB["panels"].height, "BOTTOM", UIParent, "BOTTOM", 0, 5)
 
 --------------------------------------------
 -- Panel Creation - Chat
 --------------------------------------------
-Eavu.CreatePanel(chatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
+CreateEavuPanel(chatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
 
 --------------------------------------------
 -- Panel Creation - CombatLog
 --------------------------------------------
-Eavu.CreatePanel(combatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
+CreateEavuPanel(combatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
