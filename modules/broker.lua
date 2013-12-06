efdcce7c4155395fcf9836c1629c3e8584e7143c
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-06 05:06:54 +0100

    Corrected indent (tab -> space) Some headers in comments Removed unused broker

--- modules/broker.lua
+++ modules/broker.lua
@@ -1,47 +1,39 @@
--- left side
+--------------------------------------------
+-- Left Side
+--------------------------------------------
 local guild = LibStub('LibCargoShip-2.1'):CreateBlock('SocialState', {
-	})
+  })
 guild:SetPoint("LEFT", BottomPanel, "LEFT", 10, 0)
 
 -- local friends = LibStub('LibCargoShip-2.1'):CreateBlock('Folks_Friends', {
--- 	})
+--  })
 -- friends:SetPoint("LEFT", guild, "RIGHT", 40, 0)
 
 local dura= LibStub("LibCargoShip-2.1"):CreateBlock("Durability", {
-	})
+  })
 dura:SetPoint("LEFT", guild, "RIGHT", 40, 0)
 
--- local tg = LibStub("LibCargoShip-2.1"):CreateBlock("TourGuide", {
--- 	width = 100,
--- 	})
--- tg:SetPoint("LEFT", dura, "RIGHT", 40, 0)
-
--- right side
+--------------------------------------------
+-- Right Side
+--------------------------------------------
 local money = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Money", {
-	})
+  })
 money:SetPoint("RIGHT", BottomPanel, "RIGHT", -10, 0)
 
 local fps = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Perf", {
-	width = 100,
-	})
+  width = 100,
+  })
 fps:SetPoint("RIGHT", money, "LEFT", -40, 0)
 
 local bags = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Bags", {
-	})
+  })
 bags:SetPoint("RIGHT", fps, "LEFT", -40, 0)
 
-if select(2, UnitClass("player")) == "SHAMAN" then
-	local ankh = LibStub("LibCargoShip-2.1"):CreateBlock("AnkhUp", {
-		})
-	ankh:SetPoint("RIGHT", bags, "LEFT", -40, 0)
-end
-
--- centered
+--------------------------------------------
+-- Center No Side
+--------------------------------------------
 local dps = LibStub('LibCargoShip-2.1'):CreateBlock('DPS', {fontSize=10})
 dps:SetPoint("RIGHT", BottomPanel, "CENTER", -250, 0)
-
-local group = LibStub("LibCargoShip-2.1"):CreateBlock("picoGroup", {fontSize = 10})
-group:SetPoint("LEFT", BottomPanel, "CENTER", 250, 0)
 
 local cbt = LibStub('LibCargoShip-2.1'):CreateBlock('CombatLogToggle', {fontSize=10})
 cbt:SetPoint("CENTER", BottomPanel, "CENTER")
