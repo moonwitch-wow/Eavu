0caa7011994d3721179480e71d3bcf47504865c9
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-12 04:36:24 +0100

    Moving the Quest Watcher Frame

--- modules/quest_stuff.lua
+++ modules/quest_stuff.lua
@@ -1,80 +1,80 @@
-﻿local questtags, tags = {}, {Elite = "+", Group = "G", Dungeon = "D", Raid = "R", PvP = "P", Daily = "•", Heroic = "H", Repeatable = "∞"}
+﻿-- local questtags, tags = {}, {Elite = "+", Group = "G", Dungeon = "D", Raid = "R", PvP = "P", Daily = "•", Heroic = "H", Repeatable = "∞"}
 
-local function GetTaggedTitle(i)
-  local name, level, tag, group, header, _, complete, daily = GetQuestLogTitle(i)
-  if header or not name then return end
+-- local function GetTaggedTitle(i)
+--   local name, level, tag, group, header, _, complete, daily = GetQuestLogTitle(i)
+--   if header or not name then return end
 
-  if not group or group == 0 then group = nil end
-  return string.format("[%s%s%s%s] %s", level, tag and tags[tag] or "", daily and tags.Daily or "",group or "", name), tag, daily, complete
-end
+--   if not group or group == 0 then group = nil end
+--   return string.format("[%s%s%s%s] %s", level, tag and tags[tag] or "", daily and tags.Daily or "",group or "", name), tag, daily, complete
+-- end
 
--- Add tags to the quest log
-local function QuestLog_Update()
-  for i,butt in pairs(QuestLogScrollFrame.buttons) do
-    local qi = butt:GetID()
-    local title, tag, daily, complete = GetTaggedTitle(qi)
-    if title then butt:SetText("  "..title) end
-    if (tag or daily) and not complete then butt.tag:SetText("") end
-    QuestLogTitleButton_Resize(butt)
-  end
-end
-hooksecurefunc("QuestLog_Update", QuestLog_Update)
-hooksecurefunc(QuestLogScrollFrame, "update", QuestLog_Update)
+-- -- Add tags to the quest log
+-- local function QuestLog_Update()
+--   for i,butt in pairs(QuestLogScrollFrame.buttons) do
+--     local qi = butt:GetID()
+--     local title, tag, daily, complete = GetTaggedTitle(qi)
+--     if title then butt:SetText("  "..title) end
+--     if (tag or daily) and not complete then butt.tag:SetText("") end
+--     QuestLogTitleButton_Resize(butt)
+--   end
+-- end
+-- hooksecurefunc("QuestLog_Update", QuestLog_Update)
+-- hooksecurefunc(QuestLogScrollFrame, "update", QuestLog_Update)
 
--- Add tags to the quest watcher
-hooksecurefunc("WatchFrame_Update", function()
-  local questWatchMaxWidth, watchTextIndex = 0, 1
+-- -- Add tags to the quest watcher
+-- hooksecurefunc("WatchFrame_Update", function()
+--   local questWatchMaxWidth, watchTextIndex = 0, 1
 
-  for i=1,GetNumQuestWatches() do
-    local qi = GetQuestIndexForWatch(i)
-    if qi then
-      local numObjectives = GetNumQuestLeaderBoards(qi)
+--   for i=1,GetNumQuestWatches() do
+--     local qi = GetQuestIndexForWatch(i)
+--     if qi then
+--       local numObjectives = GetNumQuestLeaderBoards(qi)
 
-      if numObjectives > 0 then
-        for bi,butt in pairs(WATCHFRAME_QUESTLINES) do
-          if butt.text:GetText() == GetQuestLogTitle(qi) then butt.text:SetText(GetTaggedTitle(qi)) end
-        end
-      end
-    end
-  end
-end)
+--       if numObjectives > 0 then
+--         for bi,butt in pairs(WATCHFRAME_QUESTLINES) do
+--           if butt.text:GetText() == GetQuestLogTitle(qi) then butt.text:SetText(GetTaggedTitle(qi)) end
+--         end
+--       end
+--     end
+--   end
+-- end)
 
--- Add tags to quest links in chat
-local function filter(self, event, msg, ...)
-  if msg then
-    return false, msg:gsub("(|c%x+|Hquest:%d+:(%d+))", "(%2) %1"), ...
-  end
-end
-for _,event in pairs{"SAY", "GUILD", "GUILD_OFFICER", "WHISPER", "WHISPER_INFORM", "PARTY", "RAID", "RAID_LEADER", "BATTLEGROUND", "BATTLEGROUND_LEADER"} do ChatFrame_AddMessageEventFilter("CHAT_MSG_"..event, filter) end
+-- -- Add tags to quest links in chat
+-- local function filter(self, event, msg, ...)
+--   if msg then
+--     return false, msg:gsub("(|c%x+|Hquest:%d+:(%d+))", "(%2) %1"), ...
+--   end
+-- end
+-- for _,event in pairs{"SAY", "GUILD", "GUILD_OFFICER", "WHISPER", "WHISPER_INFORM", "PARTY", "RAID", "RAID_LEADER", "BATTLEGROUND", "BATTLEGROUND_LEADER"} do ChatFrame_AddMessageEventFilter("CHAT_MSG_"..event, filter) end
 
--- Add tags to gossip frame
-local i
-local TRIVIAL, NORMAL = "|cff%02x%02x%02x[%d%s%s]|r "..TRIVIAL_QUEST_DISPLAY, "|cff%02x%02x%02x[%d%s%s]|r ".. NORMAL_QUEST_DISPLAY
-local function helper(isActive, ...)
-  local num = select('#', ...)
-  if num == 0 then return end
+-- -- Add tags to gossip frame
+-- local i
+-- local TRIVIAL, NORMAL = "|cff%02x%02x%02x[%d%s%s]|r "..TRIVIAL_QUEST_DISPLAY, "|cff%02x%02x%02x[%d%s%s]|r ".. NORMAL_QUEST_DISPLAY
+-- local function helper(isActive, ...)
+--   local num = select('#', ...)
+--   if num == 0 then return end
 
-  local skip = isActive and 4 or 5
+--   local skip = isActive and 4 or 5
 
-  for j=1,num,skip do
-    local title, level, isTrivial, daily, repeatable = select(j, ...)
-    if isActive then daily, repeatable = nil end
-    if title and level and level ~= -1 then
-      local color = GetQuestDifficultyColor(level)
-      _G["GossipTitleButton"..i]:SetFormattedText(isActive and isTrivial and TRIVIAL or NORMAL, color.r*255, color.g*255, color.b*255, level, repeatable and tags.Repeatable or "", daily and tags.Daily or "", title)
-    end
-    i = i + 1
-  end
-  i = i + 1
-end
+--   for j=1,num,skip do
+--     local title, level, isTrivial, daily, repeatable = select(j, ...)
+--     if isActive then daily, repeatable = nil end
+--     if title and level and level ~= -1 then
+--       local color = GetQuestDifficultyColor(level)
+--       _G["GossipTitleButton"..i]:SetFormattedText(isActive and isTrivial and TRIVIAL or NORMAL, color.r*255, color.g*255, color.b*255, level, repeatable and tags.Repeatable or "", daily and tags.Daily or "", title)
+--     end
+--     i = i + 1
+--   end
+--   i = i + 1
+-- end
 
-local function GossipUpdate()
-  i = 1
-  helper(false, GetGossipAvailableQuests()) -- name, level, trivial, daily, repeatable
-  helper(true, GetGossipActiveQuests()) -- name, level, trivial, complete
-end
-hooksecurefunc("GossipFrameUpdate", GossipUpdate)
-if GossipFrame:IsShown() then GossipUpdate() end
+-- local function GossipUpdate()
+--   i = 1
+--   helper(false, GetGossipAvailableQuests()) -- name, level, trivial, daily, repeatable
+--   helper(true, GetGossipActiveQuests()) -- name, level, trivial, complete
+-- end
+-- hooksecurefunc("GossipFrameUpdate", GossipUpdate)
+-- if GossipFrame:IsShown() then GossipUpdate() end
 
 --make the quest watchframe movable
 WatchFrame:SetClampedToScreen(true)
@@ -84,7 +84,7 @@ WatchFrame:ClearAllPoints()
 WatchFrame.ClearAllPoints = function() end
 WatchFrame:SetPoint("TOPLEFT", UIParent, "TOPLEFT", 5, -10)
 WatchFrame.SetPoint = function() end
-WatchFrame:SetHeight(600)
+WatchFrame:SetHeight(UIParent:GetHeight() - 300)
 WatchFrameHeader:EnableMouse(true)
 WatchFrameHeader:RegisterForDrag("LeftButton")
 WatchFrameHeader:SetHitRectInsets(-15, -15, -5, -5)
@@ -95,4 +95,51 @@ end)
 WatchFrameHeader:SetScript("OnDragStop", function(s)
   local f = s:GetParent()
   f:StopMovingOrSizing()
+end)
+
+-- Quicker Abandonning Quests
+local enabled = true
+local f = CreateFrame("frame",nil)
+f.d = 0
+
+local function AddQueue()
+	if (f.d + 1) <= 1 then
+		f.d = f.d + 1
+		enabled = false
+		f:SetScript("OnUpdate",function(self,elapsed)
+			f.d = f.d - elapsed
+			if f.d < 0 then
+				f:SetScript("OnUpdate",nil)
+				enabled = true
+				f.d = 0
+			end
+		end)
+	else
+		return
+	end
+end
+
+hooksecurefunc("QuestLogTitleButton_OnClick", function(self,button)
+	local questIndex = self:GetID();
+	local questName = self:GetText();
+	if ( IsModifiedClick() ) then
+		if ( self.isHeader ) then
+			return;
+		end
+		if enabled then
+			AddQueue()
+			if IsControlKeyDown() then
+				QuestLog_SetSelection(questIndex)
+				print("|cffffff00Quest abandoned: "..GetQuestLink(questIndex))
+				AbandonQuest();
+				QuestLog_Update();
+				QuestLog_SetSelection(questIndex);
+			elseif IsAltKeyDown() then
+				QuestLog_SetSelection(questIndex)
+				if GetQuestLogPushable() then
+					QuestLogPushQuest();
+				end
+			end
+		end
+	end
 end)
