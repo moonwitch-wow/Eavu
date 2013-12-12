02ec55a914dce3ba789a539cf1744adf3b0da922
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-12 04:33:07 +0100

    made Eavu more dynamic

--- modules/panels.lua
+++ modules/panels.lua
@@ -4,14 +4,15 @@ local __, Eavu = ...
 -- Panel Creation - Bottom
 -- Syntax Eavu.CreatePanel(frameName, width, height, anchor1, parent, anchor2, x, y)
 --------------------------------------------
-CreateEavuPanel("BottomPanel" , UIParent:GetWidth()-10, EavuDB["panels"].height, "BOTTOM", UIParent, "BOTTOM", 0, 5)
+CreateEavuPanel("BottomPanel" , UIParent:GetWidth()-10, UIParent:GetHeight()/45, "BOTTOM", UIParent, "BOTTOM", 0, 5)
 
 --------------------------------------------
 -- Panel Creation - Chat
 --------------------------------------------
-CreateEavuPanel(chatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
+CreateEavuPanel(chatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/7), "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
 
 --------------------------------------------
 -- Panel Creation - CombatLog
 --------------------------------------------
-CreateEavuPanel(combatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
+CreateEavuPanel(combatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/7), "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
+-- CreateEavuPanel(combatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
