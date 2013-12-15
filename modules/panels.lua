1c52529f0f4f2e2074ffcb11b978f20525929ff1
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-15 05:23:15 +0100

    changed panel size

--- modules/panels.lua
+++ modules/panels.lua
@@ -9,10 +9,10 @@ CreateEavuPanel("BottomPanel" , UIParent:GetWidth()-10, UIParent:GetHeight()/45,
 --------------------------------------------
 -- Panel Creation - Chat
 --------------------------------------------
-CreateEavuPanel(chatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/7), "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
+CreateEavuPanel(chatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/6), "BOTTOMLEFT", "BottomPanel", "TOPLEFT", 0, 5)
 
 --------------------------------------------
 -- Panel Creation - CombatLog
 --------------------------------------------
-CreateEavuPanel(combatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/7), "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
+CreateEavuPanel(combatPanel, UIParent:GetWidth()/3.25, math.floor(UIParent:GetHeight()/6), "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
 -- CreateEavuPanel(combatPanel, EavuDB["panels"].width*1.3, EavuDB["panels"].height*7, "BOTTOMRIGHT", "BottomPanel", "TOPRIGHT", 0, 5)
