19b71001868dce3cd814116ffa162dbaecf89e74
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-30 21:04:04 +0100

    Parenting changed so I can hide it in combat.

--- modules/broker.lua
+++ modules/broker.lua
@@ -1,39 +1,56 @@
+------------------------------------------------------------------------
+-- Dataobjectifying
+------------------------------------------------------------------------
+local LCS = LibStub("LibCargoShip-2.1")
+
 --------------------------------------------
 -- Left Side
 --------------------------------------------
-local guild = LibStub('LibCargoShip-2.1'):CreateBlock('|cFFFFB366Ara|r Guild', {
+local guild = LCS:CreateBlock('|cFFFFB366Ara|r Guild', {
+  parent = BottomPanel,
   })
 guild:SetPoint("LEFT", BottomPanel, "LEFT", 10, 0)
 
-local friends = LibStub('LibCargoShip-2.1'):CreateBlock('|cFFFFB366Ara|r Friends', {
+local friends = LCS:CreateBlock('|cFFFFB366Ara|r Friends', {
+  parent = BottomPanel,
  })
 friends:SetPoint("LEFT", guild, "RIGHT", 40, 0)
 
-local dura= LibStub("LibCargoShip-2.1"):CreateBlock("Durability", {
+local dura= LCS:CreateBlock("Durability", {
+  parent = BottomPanel,
   })
 dura:SetPoint("LEFT", friends, "RIGHT", 40, 0)
 
 --------------------------------------------
 -- Right Side
 --------------------------------------------
-local money = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Money", {
+local money = LCS:CreateBlock("Eavu_Money", {
+  parent = BottomPanel,
   })
 money:SetPoint("RIGHT", BottomPanel, "RIGHT", -10, 0)
 
-local fps = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Perf", {
+local fps = LCS:CreateBlock("Eavu_Perf", {
+  parent = BottomPanel,
   width = 100,
   })
 fps:SetPoint("RIGHT", money, "LEFT", -40, 0)
 
-local bags = LibStub("LibCargoShip-2.1"):CreateBlock("Eavu_Bags", {
+local bags = LCS:CreateBlock("Eavu_Bags", {
+  parent = BottomPanel,
   })
 bags:SetPoint("RIGHT", fps, "LEFT", -40, 0)
 
 --------------------------------------------
 -- Center No Side
 --------------------------------------------
-local dps = LibStub('LibCargoShip-2.1'):CreateBlock('DPS', {fontSize=10})
+local dps = LCS:CreateBlock('DPS', {
+  fontSize=10,
+  parent = BottomPanel,
+  })
 dps:SetPoint("RIGHT", BottomPanel, "CENTER", -250, 0)
 
-local cbt = LibStub('LibCargoShip-2.1'):CreateBlock('CombatLogToggle', {fontSize=10})
+local cbt = LCS:CreateBlock('CombatLogToggle', {
+  fontSize=10,
+  parent = BottomPanel,
+  })
 cbt:SetPoint("CENTER", BottomPanel, "CENTER")
