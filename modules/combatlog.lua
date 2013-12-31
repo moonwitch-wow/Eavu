be095567acf683606476977833f62caf127a7f4b
Kelly Crabbé <kelly.crabbe@gmail.com>
2013-12-31 02:00:43 +0100

    combatlog hides on combat

--- modules/combatlog.lua
+++ modules/combatlog.lua
@@ -1,3 +1,6 @@
+local handler = {}
+local frame = CreateFrame("Frame")
+
 LibStub("LibDataBroker-1.1"):NewDataObject("CombatLogToggle", {
   type = "launcher",
   icon = [[Interface\Icons\INV_Weapon_ShortBlade_01]],
@@ -16,3 +19,23 @@ LibStub("LibDataBroker-1.1"):NewDataObject("CombatLogToggle", {
     end
   end
 })
+
+function handler.PLAYER_REGEN_DISABLED(...)
+  COMBATLOG:Hide()
+  _G[COMBATLOG:GetName().."Tab"]:Hide()
+  -- BottomPanel:Hide()
+end
+
+function handler.PLAYER_REGEN_ENABLED(...)
+  COMBATLOG:Show()
+  _G[COMBATLOG:GetName().."Tab"]:Show()
+  -- BottomPanel:Show()
+end
+
+for k, v in pairs(handler) do
+ frame:RegisterEvent(k); -- Register all events for which handlers have been defined
+end
+
+frame:SetScript("OnEvent", function(self, event, ...)
+ handler[event](self, ...); -- call one of the functions above
+end)
