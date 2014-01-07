126b3d36add512130d2796dac2ecc21dc419e8cd
Kelly Crabbé <kelly.crabbe@gmail.com>
2014-01-07 03:41:11 +0100

    added my currently setup

--- modules/slashcommands.lua
+++ modules/slashcommands.lua
@@ -1,3 +1,4 @@
+local addonName, ns = ...
 -- Slash Commands
 SlashCmdList['RELOAD_UI'] = function() ReloadUI() end
 SLASH_RELOAD_UI1 = '/rl'
@@ -9,4 +10,9 @@ SLASH_PROF1 = '/prof'
 SLASH_PROF2 = '/professions'
 SlashCmdList.PROF = function()
   ToggleSpellBook(BOOKTYPE_PROFESSION)
+end
+
+SLASH_TEST1 = '/uitest'
+SlashCmdList.TEST = function()
+  print(addonName)
 end
